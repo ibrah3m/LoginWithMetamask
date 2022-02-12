254c6d6a9a88aa2f4257e04ce69b1a0a167f2f4c
ibrah3m <ibrahem.developer@gmail.com>
2022-02-12 22:41:05 +0300

    Update MetamaskControrller.php

--- src/MetamaskControrller.php
+++ src/MetamaskControrller.php
@@ -15,7 +15,7 @@ class MetamaskControrller extends Controller
 
 public function JsScript(){
 
-    return response()->view('LoginWithMetamsk::FrontJsHook')
+    return response()->view('loginwithmetamsk::FrontJsHook')
     ->header('Content-Type', 'application/javascript');
 
     
