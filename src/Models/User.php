da854e73c6722cda77af1edcfa75fb227190dfb1
ibrah3m <ibrahem.developer@gmail.com>
2022-02-12 22:52:09 +0300

    Update User.php

--- src/Models/User.php
+++ src/Models/User.php
@@ -2,48 +2,21 @@
 
 namespace ibrah3m\loginwithmetamsk\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+
 use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+ 
+    
     protected $fillable = [
-        'name',
-        'email',
-        'password',
         'public_address',
         'nonce',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
-    protected $hidden = [
-        'password',
-        'remember_token',
-    ];
+   
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-    ];
-    
+
     public function Renonce()
     {
         $digits=10;
