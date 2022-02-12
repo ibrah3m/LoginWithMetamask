81b0426d7dde57bbb2b6ebe0c39392462c22ede8
ibrah3m <ibrahem.developer@gmail.com>
2022-02-12 23:00:07 +0300

    Update MetamaskControrller.php

--- src/MetamaskControrller.php
+++ src/MetamaskControrller.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Elliptic\EC;
 use kornrunner\Keccak;
-use Ibrah3m\LoginWithMetamsk\User;
+use Ibrah3m\LoginWithMetamsk\Models\User;
 use Illuminate\Contracts\Auth\Authenticatable;
 
 class MetamaskControrller extends Controller
