49033d3cf324e9f28e3a0322dc6f0fa82c216dcb
ibrah3m <ibrahem.developer@gmail.com>
2022-02-21 15:44:48 +0300

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -1,5 +1,6 @@
 <?php
 
+Route::group(['middleware' => ['web']], function () {
 
 Route::get('/CustomLoginPage',function(){
     return view('loginwithmetamsk::CustomLoginPage');
@@ -7,3 +8,4 @@ Route::get('/CustomLoginPage',function(){
 Route::get('/JsPlugin',[Ibrah3m\LoginWithMetamsk\MetamaskControrller::class,'JsScript'])->name('metamask.plugin') ;
 Route::post('/Nonce',[Ibrah3m\LoginWithMetamsk\MetamaskControrller::class,'Nonce'])->name('metamask.nonce');
 Route::post('/SignatureVerification',[Ibrah3m\LoginWithMetamsk\MetamaskControrller::class,'SignatureVerification'])->name('metamask.signature');
+});
